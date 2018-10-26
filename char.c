9aef370f140cf05de7f4dea15667ac84dee0889e
Rodolfo Gaspar <dolfogaspar@gmail.com>
2018-10-25 23:28:46 -0300

    adicionado testes para unsigned char

--- char.c
+++ char.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 
 int main(void){
-  char c;
+  signed char sc;
+  unsigned char uc;
 
-  printf("O tamanho de c eh: %lu byte\n", sizeof c);
-  printf("O tamanho de c eh: %lu byte\n", sizeof (char));
-  printf("O tamanho de c eh: %lu byte / %lu bits\n", sizeof c, sizeof c * 8);
+  printf("O tamanho de sc eh: %lu byte\n", sizeof sc);
+  printf("O tamanho de sc eh: %lu byte\n", sizeof (char));
+  printf("O tamanho de sc eh: %lu byte / %lu bits\n", sizeof sc, sizeof sc * 8);
 
-  c=255;
-  printf("O valor de c eh %i\n", c);
+  sc=255;
+  printf("O valor de sc eh: %i\n", sc);
 
-  c=127;
-  printf("O valor de c eh: %i\n", c);
+  sc=127;
+  printf("O valor de sc eh: %i\n", sc);
 
-  c=128;
-  printf("O valor de c eh: %i\n", c);
+  sc=128;
+  printf("O valor de sc eh: %i\n", sc);
+
+  uc = 255;
+  printf("O valor de uc eh: %i\n", uc);
+
+  uc=127;
+  printf("O valor de uc eh: %i\n", uc);
+
+  uc=128;
+  printf("O valor de uc eh: %i\n", uc);
 
   return 0;
 }
